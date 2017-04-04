1d90f527ebf2be3d1e7ed4d3510914c6520685f5
kalynndenton <dentonkalynn12@gmail.com>
2017-04-04 02:26:00 -0400

    added comments and more notes

--- Week2_day1_DoIt/Program.cs
+++ Week2_day1_DoIt/Program.cs
@@ -12,15 +12,17 @@ namespace Week2_day1_DoIt
         {
             string[] vacaSpots = { "Hawaii", "Narita", "Orlando", "Kyoto", "Hakone", "Osaka", "Kamaura", "Yokohama", "Ishigaki", "Shikoku" };
 
-            Console.WriteLine(vacaSpots[0] + " " +  vacaSpots[9]);
+            Console.WriteLine(vacaSpots[0] + " " +  vacaSpots[9]);//Choose ten vacation spots, then print the first and last
 
-            int[] childrenAge = { 3, 6, 1 };
+            int[] childrenAge = { 3, 6, 1 };//get 3 childrens ages and print them.
 
             Console.WriteLine("Caleb's age: " + childrenAge[0]);
             Console.WriteLine("June's age: " + childrenAge[1]);
             Console.WriteLine("Autumn's age: " + childrenAge[2]);
 
-            char[] classMateFirstLetter = { 'A', 'S', 'L', 'N' };
+            int[] ages = new int[6]; //If elements are unknown. Set length of Array!
+
+            char[] classMateFirstLetter = { 'A', 'S', 'L', 'N' }; //The first letters of name of classmates
 
             foreach (char letter in classMateFirstLetter)
             {
