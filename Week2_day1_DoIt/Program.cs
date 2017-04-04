a4190b9d901b85918185f63a943587c5ebe201aa
kalynndenton <dentonkalynn12@gmail.com>
2017-04-04 02:18:12 -0400

    practicing commiting a project

---
+++ Week2_day1_DoIt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_day1_DoIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vacaSpots = { "Hawaii", "Narita-sen", "Orlando", "Kyoto", "Hakone", "Osaka", "Kamaura", "Yokohama", "Ishigaki", "Shikoku" };
+
+            Console.WriteLine(vacaSpots[0] + " " +  vacaSpots[9]);
+
+            int[] childrenAge = { 3, 6, 1 };
+
+            Console.WriteLine("King's age: " + childrenAge[0]);
+            Console.WriteLine("Journee's age: " + childrenAge[1]);
+            Console.WriteLine("Autumn's age: " + childrenAge[2]);
+
+            char[] classMateFirstLetter = { 'A', 'S', 'L', 'N' };
+
+            foreach (char letter in classMateFirstLetter)
+            {
+                Console.WriteLine(letter);
+                
+            }
+        }
+    }
+}
